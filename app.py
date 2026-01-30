8261b69a3d04ef4661c0bad36db10f21dded56b9
abrahemnas28-design <abrahem.nas28@gmail.com>
2026-01-30 16:53:52 +0200

    Correct title and initialize session state properly
    Fix typo in title and correct session state initialization.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.tittle("המונה של אברהים")
-if 'count' not in st.session_state
-      st.session_state=0
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 st.write(f"המספר הנוכחי הוא: {st.session_state.count}")
 
 if st.button("לחץ כאן כדי להוסיף 1"):
