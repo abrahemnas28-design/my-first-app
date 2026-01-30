40bbdd363d253ba214c9e2f4db626e40eebd3a84
abrahemnas28-design <abrahem.nas28@gmail.com>
2026-01-30 17:34:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+
 import streamlit as st
 
-user_name = st.text_input("נא להזין שם עובד:")
+st.title("מעקב תיקון תקלות")
+
+# שלב הזיכרון - האם יש לנו ארגז בשם repairs?
+if 'repairs' not in st.session_state:
+    st.session_state.repairs = 0
+
+# הצגת המשתנה למשתמש
+st.subheader(f"סה"כ תקלות שטופלו היום: {st.session_state.repairs}")
+
+# הפעולה - כפתור שמעדכן את המשתנה
+if st.button("✅ סימנתי תקלה כטופלה"):
+    st.session_state.repairs += 1  # כאן אנחנו מוסיפים 1 למשתנה המספרי
+    st.success("כל הכבוד! העדכון נשמר")
+    st.rerun()
